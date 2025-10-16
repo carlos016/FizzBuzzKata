69129477f1498e7c7a55894327daf65a0a99390b
Carlos García González <alu0100898026@ull.edu.es>
2025-10-16 15:55:23 +0100

    Implement FizzBuzz.compute

--- Python/src/fizzbuzz.py
+++ Python/src/fizzbuzz.py
@@ -20,3 +20,21 @@ class FizzBuzz:
             raise ValueError("Limit/Number to compute must be a positive integer greater than zero.")
         return True
     
+    # Método para calcular el valor FizzBuzz de un número
+    def compute(self, number):
+        # Validamos el número, solo lo hacemos si es válido
+        #if self.is_valid_number(number):
+            result = ""
+            # Si el número contiene 3 o 5, añadimos Fizz o Buzz respectivamente
+            if '3' in str(number):
+                result += "Fizz"
+            if '5' in str(number):
+                result += "Buzz"
+            # Si el número es múltiplo de 3 o 5, añadimos Fizz o Buzz respectivamente
+            if number % 3 == 0:
+                result += "Fizz"
+            if number % 5 == 0:
+                result += "Buzz"
+            # Si no se ha añadido nada, devolvemos el número como cadena
+            return result if result else str(number)
+    
